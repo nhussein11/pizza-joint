2d2c9b0c23322f27443a76a8d24b03d39f7a6099
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-05-26 16:58:16 -0300

    fix: fixing types and signatures on pizzaReducer

--- src/reducers/pizzaReducer.ts
+++ src/reducers/pizzaReducer.ts
@@ -1,9 +1,15 @@
 import { PizzaAction, PizzaActionType } from '../@types/actions';
 import { PizzaState } from '../@types/state';
 
-const pizzaReducer = (state: PizzaState, action: PizzaAction) => {
+const pizzaReducer = (state: PizzaState, action: PizzaAction): PizzaState => {
   switch (action.type) {
     case PizzaActionType.ADD_BASE:
+      if (typeof action.payload !== 'string') {
+        return state;
+      }
+      if (state.pizza.base === action.payload) {
+        return state;
+      }
       return {
         ...state,
         pizza: {
@@ -12,6 +18,9 @@ const pizzaReducer = (state: PizzaState, action: PizzaAction) => {
         },
       };
     case PizzaActionType.HANDLE_TOPPING:
+      if (typeof action.payload !== 'string') {
+        return state;
+      }
       if (state.pizza.toppings.includes(action.payload)) {
         const newToppings = state.pizza.toppings.filter(
           topping => topping !== action.payload
