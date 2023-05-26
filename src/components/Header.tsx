32a9e19e6ded34c871d17c83d205a8253577ba35
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-05-26 15:48:57 -0300

    feat: adding svg animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,34 @@
 import { motion } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
 
+const svgVariant = {
+  hidden: { rotate: -180 },
+  visible: {
+    rotate: 0,
+    transition: { duration: 1 },
+  },
+};
+
 const Header = () => {
   const navigate = useNavigate();
 
   return (
     <header>
       <div className="logo">
-        <svg
+        <motion.svg
           className="pizza-svg"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 100 100"
+          variants={svgVariant}
+          initial="hidden"
+          animate="visible"
         >
           <path
             fill="none"
             d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
           />
           <path fill="none" d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z" />
-        </svg>
+        </motion.svg>
       </div>
       <motion.div
         initial={{ y: -250 }}
